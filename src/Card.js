4ec6bfc99ca4041edf05497606a9290455d50c40
Sunny /dx/ <sunnyredmi5a@gmail.com>
2021-08-06 10:22:22 +0000

    added reddit api

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,16 @@
-function Card() {
+// id={item.data.id}
+// title={item.data.title}
+// url={item.data.url}
+// author={item.data.author}
+// small_img={parent_img}
+// ups={item.data.ups}item.data.preview.images[0].resolutions[3].url
+// permalink={item.data.permalink}
+
+function Card({wall}) {
+  console.log(wall);
   return (
     <div className="">
-      <img className="object-cover object-center h-full w-full" src={`https://images.hdqwalls.com/wallpapers/angry-bird-minimal-8k-6s.jpg`} alt=""></img>
+      <img className="object-cover object-center h-full w-full" src={`${wall.preview}`} alt=""></img>
       <div>
         <div class="text-lg select-none text-black">Angry Bird</div>
       </div>
--- src/Wallpapers.js
+++ src/Wallpapers.js
@@ -1,20 +1,40 @@
 import Card from "./Card"
-function Wallpapers() {
+import axios from "axios"
+import { useState, useEffect } from 'react'
+
+function Wallpapers({ data }) {
+  const [walls, setWalls] = useState([])
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get('https://www.reddit.com/r/wallpaper/hot.json?&limit=10&raw_json=1')
+      const resultData = []
+      result.data.data.children.map((post) => (
+        resultData.push({
+          preview:post.data.preview.images[0].resolutions[3].url,
+          title:post.data.title,
+          author:post.data.author,
+          ups:post.data.ups
+        })
+      ))
+      // result.data.data.children.forEach((child) => {
+      //   resultData.push({
+      //     preview:child.data.preview.images[0].resolutions[3].url
+      //   });
+      // });
+      console.log(resultData)
+      setWalls(resultData)
+      }
+      fetchItems()
+  }, [])
+  
   return (
     <div>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
-          <li className="bg-white rounded-lg shadow-xl overflow-hidden"><Card /></li>
+          {walls.map((post) =>
+            <li className="bg-white rounded-lg shadow-xl overflow-hidden">
+              <Card wall={post}/>
+            </li>
+          )}
         </ul>
     </div>
   );
